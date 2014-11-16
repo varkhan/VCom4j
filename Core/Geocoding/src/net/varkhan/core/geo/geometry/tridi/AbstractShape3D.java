e0957e5e22fef90aed82737ff11fed7725d3c04b
Varkhan <varkhan@varkhan.net>
2014-11-15 22:00:06 -0800

    Geocoding:  - support for 3d shapes

---
+++ Core/Geocoding/src/net/varkhan/core/geo/geometry/tridi/AbstractShape3D.java
@@ -0,0 +1,83 @@
+package net.varkhan.core.geo.geometry.tridi;
+
+import net.varkhan.core.geo.geometry.AbstractShape;
+
+
+/**
+ * <b></b>.
+ * <p/>
+ *
+ * @author varkhan
+ * @date 9/3/12
+ * @time 3:12 PM
+ */
+public abstract class AbstractShape3D extends AbstractShape implements Shape3D {
+
+    public int dim() { return 2; }
+
+    public double cmin(int d) {
+        switch(d) {
+            case 0: return xmin();
+            case 1: return ymin();
+            default: return 0;
+        }
+    }
+    public double cmax(int d) {
+        switch(d) {
+            case 0: return xmax();
+            case 1: return ymax();
+            default: return 0;
+        }
+    }
+    public double[] cmin() { return new double[] { xmin(), ymin() }; }
+    public double[] cmax() { return new double[] { xmax(), ymax() }; }
+
+    public double cctr(int d) {
+        switch(d) {
+            case 0: return xctr();
+            case 1: return yctr();
+            default: return 0;
+        }
+    }
+    public double[] cctr() { return new double[] { xctr(), yctr() }; }
+    public Point3D ctr() { return new AbstractPoint3D() {
+        public double xctr() { return AbstractShape3D.this.xctr(); }
+        public double yctr() { return AbstractShape3D.this.yctr(); }
+        public double zctr() { return AbstractShape3D.this.zctr(); }
+    }; }
+
+
+    @Override
+    public double dmin2(double... point) {
+        double x = point.length>=1?point[0]:0;
+        double y = point.length>=2?point[1]:0;
+        double dist2 = dmin2(x, y);
+        int i=2;
+        while(i<point.length) {
+            double delta = point[i];
+            dist2 += delta*delta;
+            i ++;
+        }
+        return dist2;
+    }
+
+    @Override
+    public double dmax2(double... point) {
+        double x = point.length>=1?point[0]:0;
+        double y = point.length>=2?point[1]:0;
+        double dist2 = dmax2(x, y);
+        int i=2;
+        while(i<point.length) {
+            double delta = point[i];
+            dist2 += delta*delta;
+            i ++;
+        }
+        return dist2;
+    }
+
+    public boolean contains(double x, double y, double z) { return dmin2(x, y, z)==0; }
+
+    public double dmax(double x, double y, double z) { return Math.sqrt(dmax2(x, y, z)); }
+    public double dmin(double x, double y, double z) { return Math.sqrt(dmin2(x, y, z)); }
+
+}
---
+++ Core/Geocoding/src/net/varkhan/core/geo/geometry/tridi/RectF3D.java
@@ -0,0 +1,146 @@
+package net.varkhan.core.geo.geometry.tridi;
+
+/**
+ * <b></b>.
+ * <p/>
+ *
+ * @author varkhan
+ * @date 9/3/12
+ * @time 3:31 PM
+ */
+public class RectF3D extends AbstractShape3D implements Rect3D {
+
+    protected float xmin;
+    protected float xmax;
+    protected float ymin;
+    protected float ymax;
+    protected float zmin;
+    protected float zmax;
+
+    public RectF3D(float xmin, float xmax, float ymin, float ymax, float zmin, float zmax) {
+        this.xmin=xmin;
+        this.xmax=xmax;
+        this.ymin=ymin;
+        this.ymax=ymax;
+        this.zmin=zmin;
+        this.zmax=zmax;
+    }
+
+    public RectF3D(Shape3D shape) {
+        this((float)shape.xmin(),(float)shape.xmax(),(float)shape.ymin(),(float)shape.ymax(),(float)shape.zmin(),(float)shape.zmax());
+    }
+
+    public RectF3D(Point3D point, float xdel, float ydel, float zdel) {
+        this((float)point.xctr()-xdel,(float)point.xctr()+xdel,(float)point.yctr()-ydel,(float)point.yctr()+ydel,(float)point.zctr()-zdel,(float)point.zctr()+zdel);
+    }
+
+    public double xmin() { return xmin; }
+    public double xmax() { return xmax; }
+    public double ymin() { return ymin; }
+    public double ymax() { return ymax; }
+    public double zmin() { return zmin; }
+    public double zmax() { return zmax; }
+
+    @Override
+    public double xctr() {
+        return 0.5*(xmin+xmax);
+    }
+
+    @Override
+    public double yctr() {
+        return 0.5*(ymin+ymax);
+    }
+
+
+    @Override
+    public double zctr() {
+        return 0.5*(zmin+zmax);
+    }
+
+    @Override
+    public double rad2() {
+        double deltax = xmax-xmin;
+        double deltay = ymax-ymin;
+        return 0.25*(deltax*deltax+deltay*deltay);
+    }
+
+    @Override
+    public double msr() {
+        double deltax = xmax-xmin;
+        double deltay = ymax-ymin;
+        return deltax*deltay;
+    }
+
+    @Override
+    public double dmin2(double x, double y, double z) {
+        double deltax = 0;
+        if(xmin>x) deltax = xmin-x;
+        if(x>xmax) deltax = x-xmax;
+        double deltay = 0;
+        if(ymin>y) deltay = ymin-y;
+        if(y>ymax) deltay = y-ymax;
+        double deltaz = 0;
+        if(zmin>z) deltaz = zmin-z;
+        if(z>zmax) deltaz = z-zmax;
+        return deltax*deltax+deltay*deltay+deltaz*deltaz;
+    }
+
+    @Override
+    public double dmax2(double x, double y, double z) {
+        double deltax = 0;
+        double dxmin = xmin-x;
+        if(deltax<+dxmin) deltax = +dxmin;
+        if(deltax<-dxmin) deltax = -dxmin;
+        double dxmax = x-xmax;
+        if(deltax<+dxmax) deltax = +dxmax;
+        if(deltax<-dxmax) deltax = -dxmax;
+        double deltay = 0;
+        double dymin = ymin-y;
+        if(deltay<+dymin) deltay = +dymin;
+        if(deltay<-dymin) deltay = -dymin;
+        double dymax = y-ymax;
+        if(deltay<+dymax) deltay = +dymax;
+        if(deltay<-dymax) deltay = -dymax;
+        double deltaz = 0;
+        double dzmin = zmin-z;
+        if(deltaz<+dzmin) deltaz = +dzmin;
+        if(deltaz<-dzmin) deltaz = -dzmin;
+        double dzmax = z-zmax;
+        if(deltaz<+dzmax) deltaz = +dzmax;
+        if(deltaz<-dzmax) deltaz = -dzmax;
+        return deltax*deltax+deltay*deltay+deltaz*deltaz;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(this==o) return true;
+        if(o instanceof Rect3D){
+            Rect3D that=(Rect3D) o;
+            return this.xmin==that.xmin() && this.xmax==that.xmax()
+                   && this.ymin==that.ymin() && this.ymax==that.ymax()
+                   && this.zmin==that.zmin() && this.ymax==that.zmax();
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Float.floatToIntBits(xmin) + 31*Float.floatToIntBits(xmax)
+               + 61*Float.floatToIntBits(ymin) + 89*Float.floatToIntBits(ymax)
+               + 127*Float.floatToIntBits(zmin) + 151*Float.floatToIntBits(zmax);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder buf = new StringBuilder();
+        buf.append('(').append(' ');
+        buf.append('[').append(xmin).append(':').append(xmax).append(']');
+        buf.append(" x ");
+        buf.append('[').append(ymin).append(':').append(ymax).append(']');
+        buf.append(" x ");
+        buf.append('[').append(zmin).append(':').append(zmax).append(']');
+        buf.append(' ').append(')');
+        return buf.toString();
+    }
+
+}
