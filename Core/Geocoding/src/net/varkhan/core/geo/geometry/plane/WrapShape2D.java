9143a356f95c1ebeaaa04da145c0ae6d6fcc28a9
Varkhan <varkhan@varkhan.net>
2014-11-16 21:10:45 -0800

    Geocoding:  - wrap shapes

---
+++ Core/Geocoding/src/net/varkhan/core/geo/geometry/plane/WrapShape2D.java
@@ -0,0 +1,78 @@
+package net.varkhan.core.geo.geometry.plane;
+
+/**
+ * <b></b>.
+ * <p/>
+ *
+ * @author varkhan
+ * @date 11/16/14
+ * @time 9:05 PM
+ */
+public class WrapShape2D extends AbstractShape2D {
+    protected Shape2D shape;
+
+    public WrapShape2D(Shape2D shape) {
+        this.shape=shape;
+    }
+
+    @Override
+    public double xmin() {return shape.xmin();}
+
+    @Override
+    public double xmax() {return shape.xmax();}
+
+    @Override
+    public double ymin() {return shape.ymin();}
+
+    @Override
+    public double ymax() {return shape.ymax();}
+
+    @Override
+    public double xctr() {return shape.xctr();}
+
+    @Override
+    public double yctr() {return shape.yctr();}
+
+    @Override
+    public Point2D ctr() {return shape.ctr();}
+
+    @Override
+    public double rad() {return shape.rad();}
+
+    @Override
+    public double rad2() {return shape.rad2();}
+
+    @Override
+    public double msr() {return shape.msr();}
+
+    @Override
+    public boolean contains(double x, double y) {return shape.contains(x, y);}
+
+    @Override
+    public double dmin(double x, double y) {return shape.dmin(x, y);}
+
+    @Override
+    public double dmax(double x, double y) {return shape.dmax(x, y);}
+
+    @Override
+    public double dmin2(double x, double y) {return shape.dmin2(x, y);}
+
+    @Override
+    public double dmax2(double x, double y) {return shape.dmax2(x, y);}
+
+    @Override
+    public boolean contains(Point2D point) {return shape.contains(point);}
+
+    @Override
+    public double dmin(Point2D point) {return shape.dmin(point);}
+
+    @Override
+    public double dmax(Point2D point) {return shape.dmax(point);}
+
+    @Override
+    public double dmin2(Point2D point) {return shape.dmin2(point);}
+
+    @Override
+    public double dmax2(Point2D point) {return shape.dmax2(point);}
+
+}
