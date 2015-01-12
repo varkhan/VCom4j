c3acb2ada0939e9af8bf98314824855c2c936abb
Varkhan <varkhan@varkhan.net>
2015-01-11 23:26:47 -0800

    Conversion:  - ^^ tests

--- Base/Conversion/tst/net/varkhan/base/conversion/character/NumbersTest.java
+++ Base/Conversion/tst/net/varkhan/base/conversion/character/NumbersTest.java
@@ -5,14 +5,14 @@ import junit.framework.TestCase;
 import java.util.Random;
 
 
-public class NumberTest extends TestCase {
+public class NumbersTest extends TestCase {
 
     public void testParseLong() throws Exception {
         Random rand = new Random();
         for(int i=0; i<10000; i++) {
             long p = rand.nextLong();
             String s = Long.toString(p);
-            assertEquals("parse("+s+")",p,Number.parseLong(s, 0,s.length()));
+            assertEquals("parse("+s+")",p, Numbers.parseLong(s));
         }
     }
 
@@ -22,7 +22,7 @@ public class NumberTest extends TestCase {
         for(int i=0; i<10000; i++) {
             double p = rand.nextDouble()*Math.pow(10,300*(rand.nextDouble()-0.5));
             String s = Double.toString(p);
-            assertEquals("parse("+s+")",p,Number.parseDouble(s, 0,s.length()),p*1e-15);
+            assertEquals("parse("+s+")",p, Numbers.parseDouble(s),p*1e-15);
         }
     }
 
