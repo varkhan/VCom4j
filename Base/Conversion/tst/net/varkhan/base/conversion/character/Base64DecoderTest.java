86eece35b695739a0df5412581980f5f34a2f3f8
Varkhan <varkhan@varkhan.net>
2020-08-06 13:26:07 +0200

    Addl. Base64 tests

--- Base/Conversion/tst/net/varkhan/base/conversion/character/Base64DecoderTest.java
+++ Base/Conversion/tst/net/varkhan/base/conversion/character/Base64DecoderTest.java
@@ -19,6 +19,7 @@ public class Base64DecoderTest extends TestCase {
 
     public void testDecode() throws Exception {
         Base64Decoder<Object> dec = new Base64Decoder<Object>();
+        assertEquals("AA==",dec.decode(new byte[] {0},null));
         assertEquals("Rg==",dec.decode("F".getBytes(Charset.forName("US-ASCII")),null));
         assertEquals("Rm8=",dec.decode("Fo".getBytes(Charset.forName("US-ASCII")),null));
         assertEquals("Rm9vIEJhcg==",dec.decode("Foo Bar".getBytes(Charset.forName("US-ASCII")),null));
@@ -26,6 +27,7 @@ public class Base64DecoderTest extends TestCase {
         assertEquals("Rm9vIEJhcg==",dec.decode(new ByteArrayInputStream("Foo Bar".getBytes(Charset.forName("US-ASCII"))),null));
         assertEquals("Rm9vIEJhciB+",dec.decode(ByteBuffer.wrap("Foo Bar ~".getBytes(Charset.forName("US-ASCII"))),null));
         Base64Decoder<Object> decn = new Base64Decoder<Object>(Base64Decoder.CHARMAP_UTF7,Base64Decoder.NO_PADDING);
+        assertEquals("AA",decn.decode(new byte[] {0},null));
         assertEquals("Rg",decn.decode("F".getBytes(Charset.forName("US-ASCII")),null));
         assertEquals("Rm8",decn.decode("Fo".getBytes(Charset.forName("US-ASCII")),null));
         assertEquals("Rm9vIEJhcg",decn.decode("Foo Bar".getBytes(Charset.forName("US-ASCII")),null));
--- Base/Conversion/tst/net/varkhan/base/conversion/character/Base64EncoderTest.java
+++ Base/Conversion/tst/net/varkhan/base/conversion/character/Base64EncoderTest.java
@@ -19,11 +19,15 @@ public class Base64EncoderTest extends TestCase {
 
     public void testEncode() throws Exception {
         Base64Encoder<Object> enc = new Base64Encoder<Object>();
+        assertEquals(1,enc.encode("AA==",null).length);
+        assertEquals(0,enc.encode("AA==",null)[0]);
         assertEquals("F",new String(enc.encode("Rg==",null)));
         assertEquals("Fo",new String(enc.encode("Rm8=",null)));
         assertEquals("Foo Bar",new String(enc.encode("Rm9vIEJhcg==",null)));
         assertEquals("Foo Bar ~",new String(enc.encode("Rm9vIEJhciB+",null)));
         Base64Encoder<Object> encn = new Base64Encoder<Object>(Base64Decoder.CHARMAP_UTF7,Base64Decoder.NO_PADDING);
+        assertEquals(1,encn.encode("AA",null).length);
+        assertEquals(0,encn.encode("AA",null)[0]);
         assertEquals("F",new String(encn.encode("Rg",null)));
         assertEquals("Fo",new String(encn.encode("Rm8",null)));
         assertEquals("Foo Bar",new String(encn.encode("Rm9vIEJhcg",null)));
